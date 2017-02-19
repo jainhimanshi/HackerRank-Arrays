ad9be05616485919249f2b5e5710a36a7a9a1ac7
Himanshi Jain <jainhimanshi97@gmail.com>
2017-02-19 20:12:07 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,11 @@
+package feb19;
+
+public class Sum {
+	public static void main(String[] args) {
+		int sum=0;
+		int array[]={1,2,3,4,5};
+		for(int n=0;n<=array.length;n++)
+			System.out.println(sum=array[n]+sum);
+	}
+
+}
