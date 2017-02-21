e88d0c10a48ed9c2dcaa03c144e0fd4f72cd9a77
Himanshi Jain <jainhimanshi97@gmail.com>
2017-02-22 02:58:36 +0530

    Add files via upload

---
+++ StringSort.java
@@ -0,0 +1,12 @@
+package w3arraysolution;
+import java.util.*;
+public class StringSort {
+	        public static void main(String args[]){
+	                String names[]  =  {"John", "alex", "Chris", "williams", "Mark", "Bob"};
+	                Arrays.sort(names);
+	                System.out.println("String array sorted ");
+	                for(int i=0; i < names.length; i++){
+	                        System.out.println(names[i]);
+	                }
+	        }
+}
--- Sum.java
+++ Sum.java
@@ -1,11 +1,13 @@
-package feb19;
+//Java program to sum values of an array.
+package w3arraysolution;
 
 public class Sum {
 	public static void main(String[] args) {
 		int sum=0;
 		int array[]={1,2,3,4,5};
-		for(int n=0;n<=array.length;n++)
-			System.out.println(sum=array[n]+sum);
+		for(int n=0;n<array.length;n++){
+			sum=array[n]+sum;
+		}
+		System.out.println(sum);
 	}
-
 }
